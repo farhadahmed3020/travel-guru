d164934024a42c295a3625513553a02f4794bb03
farhadahmed3020 <farhadahmed3020@gmail.com>
2020-10-01 15:36:22 +0600

    add all

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+
+            <nav> 
+                
+            </nav>
+            <div> 
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Header;
